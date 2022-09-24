2bdeb05a2085fe7e24eea698e1f5b5c74e623965
allan <allangreen2906@gmail.com>
2022-09-24 15:48:30 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props:ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello React Native!</Text>
+
+      <Button title='send 1' />
+      <Button title='send 2' />
+
       <StatusBar style="auto" />
     </View>
   );
